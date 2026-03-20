4c032b4220cfb85147c28e14f9ca912b7c12dc03
JoseMG-ux <joseutp14@gmail.com>
2026-03-20 14:45:18 -0500

    refactor: cambios en la funcion para enviar emails

--- services/send_email.ts
+++ services/send_email.ts
@@ -6,14 +6,28 @@ const EMAILJS_SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
 const EMAILJS_TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
 
 export const sendContactEmail = async (requestEmail: EmailRequest) => {
-  return await emailjs.send(
-    EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPLATE_ID,
-    {
-      from_name: requestEmail.name,
-      reply_to: requestEmail.email,
-      message: requestEmail.message,
-    },
-    { publicKey: EMAILJS_PUBLIC_KEY },
-  );
+  const templateParams = {
+    from_name: requestEmail.name,
+    reply_to: requestEmail.email,
+    subject: requestEmail.subject,
+    message: requestEmail.message,
+  };
+
+  return await emailjs
+    .send(
+      EMAILJS_SERVICE_ID,
+      EMAILJS_TEMPLATE_ID,
+      templateParams,
+      EMAILJS_PUBLIC_KEY,
+    )
+    .then(
+      (response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        return response;
+      },
+      (error) => {
+        console.log('FAILED...', error);
+        return error;
+      },
+    );
 };
